a45cb7deb69c806b49761743900e12e67aa689cc
Daisaku Kato <=50142017+katkic@users.noreply.github.com>
2019-08-05 18:58:09 +0900

    コンテンツの切り替えを実装

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ $(function() {
     $('.tab li').removeClass('select');
     $(this).addClass('select');
 
+    $('.content li').addClass('hide');
+    let index = $('.tab li').index($(this));
+    $(this).removeClass('hide');
+    $('.content li').eq(index).removeClass('hide');
   });
-
 });
